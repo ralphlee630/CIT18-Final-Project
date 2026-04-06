9d70008bd1ccc5f10715592675050bf820afd098
Ralph Lee <143706910+ralphlee630@users.noreply.github.com>
2026-04-06 15:53:25 +0800

    Refactor user and task model factory calls

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,11 +1,13 @@
 <?php
 namespace Database\Seeders;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Task;
 
 class DatabaseSeeder extends Seeder {
     public function run(): void {
-        \App\Models\User::factory(3)->create()->each(function ($user) {
-            \App\Models\Task::factory(rand(5, 7))->create(['user_id' => $user->id]);
+        User::factory(3)->create()->each(function ($user) {
+            Task::factory(rand(5, 7))->create(['user_id' => $user->id]);
         });
     }
 }
